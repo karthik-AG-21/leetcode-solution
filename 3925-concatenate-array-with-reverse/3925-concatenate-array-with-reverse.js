b0099e8ab79c645eed58d33e4f56a16d626484ed
Karthik AG <karthikjr2004jr@gmail.com>
2026-06-10 13:34:54 +0530

    Time: 1 ms (85.79%), Space: 58.9 MB (57.37%) - LeetHub

---
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var concatWithReverse = function(nums) {
+    let array = [];
+    for(let i = 0; i<nums.length; i++){
+        array.push(nums[i])
+    }
+    for(let j = nums.length-1; j>=0; j--){
+        array.push(nums[j])
+    }
+    return array;
+    
+};
