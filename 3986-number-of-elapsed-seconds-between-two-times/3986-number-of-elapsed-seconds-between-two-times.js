7ec3b37f62da22e9c7b137d886aa2c8c996c60ed
Karthik AG <karthikjr2004jr@gmail.com>
2026-07-12 09:59:18 +0530

    Time: 2 ms (46.04%), Space: 55.4 MB (93.53%) - LeetHub

--- 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.js
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.js
@@ -7,12 +7,12 @@ var secondsBetweenTimes = function(startTime, endTime) {
 let newEnd = endTime.split(":") 
 newEnd[0]*=3600  
 newEnd[1]*=60 
-totalend = newEnd.reduce((sum ,item)=>sum+Number(item),0)
+let totalend = newEnd.reduce((sum ,item)=>sum+Number(item),0)
     
 let newStart = startTime.split(":")
 newStart[0]*=3600
 newStart[1]*=60
-totalstart = newStart.reduce((sum ,item)=>sum+Number(item),0)
+let totalstart = newStart.reduce((sum ,item)=>sum+Number(item),0)
 
 return totalend - totalstart
 
