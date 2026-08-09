3f8095ac3769eca276f480114ddb17924169d946
Karthik AG <karthikjr2004jr@gmail.com>
2026-08-09 15:24:16 +0530

    Time: 329 ms (5.13%), Space: 55.6 MB (87.98%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -3,9 +3,11 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
+    let n = 0;
     for(let i = 0; i<=nums.length;i++){
         if(!nums.includes(i)){
-            return i
+            n = i
         }
     }
+    return n
 };
