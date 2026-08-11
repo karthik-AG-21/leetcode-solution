589785c575dc659a3bd835a5f36a7f9db3aef33b
Karthik AG <karthikjr2004jr@gmail.com>
2026-08-11 12:42:35 +0530

    Time: 0 ms (100%), Space: 56.1 MB (89.02%) - LeetHub

---
+++ 3264-final-array-state-after-k-multiplication-operations-i/3264-final-array-state-after-k-multiplication-operations-i.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} multiplier
+ * @return {number[]}
+ */
+var getFinalState = function(nums, k, multiplier) {
+    for(let i = 0 ; i<k; i++){
+      let n = nums.indexOf(Math.min(...nums))
+     nums[n] = nums[n] * multiplier
+      
+    }
+
+    return nums
+
+    
+};
