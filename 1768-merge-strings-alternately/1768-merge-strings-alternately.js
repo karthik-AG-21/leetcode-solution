229c823eb5d9d12d1b0abf0c5acac62c0238b73c
Karthik AG <karthikjr2004jr@gmail.com>
2026-08-21 10:44:36 +0530

    Time: 42 ms (72.78%), Space: 53.6 MB (72.42%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -5,16 +5,12 @@
  */
 var mergeAlternately = function(word1, word2) {
     let array=[]
-    let newWord1 = word1.split("")
-    let newWord2 = word2.split("")
-    if(newWord1.length >newWord2.length){
-        n = newWord1.length
-    }else{
-        n = newWord2.length
-    }
+    let n1 = word1.split("")
+    let n2 = word2.split("")
+    n1.length >n2.length ? n = n1.length : n = n2.length
     for(let i = 0 ; i<n; i++){
-        array.push(newWord1[i])
-        array.push(newWord2[i])
+        array.push(n1[i])
+        array.push(n2[i])
     }
 
 return array.join("")
