e46e11ad7ea8921a81cf2afda4bc6d42863a55d6
Karthik AG <karthikjr2004jr@gmail.com>
2026-08-25 16:32:12 +0530

    Time: 14 ms (17.82%), Space: 58.8 MB (20.77%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -6,11 +6,9 @@ var singleNumber = function(nums) {
    let obj = {}
 
    for(let i = 0; i<nums.length;i++){
-    if(obj[nums[i]]){
-        obj[nums[i]]++
-    }else{
-        obj[nums[i]] = 1;
-    }
+   
+
+    obj[nums[i]] ? obj[nums[i]]++ :  obj[nums[i]] = 1;
    }
 
   
