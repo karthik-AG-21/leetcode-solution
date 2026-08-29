d7e44e88af79a8cba60d93b24dbddcb22a39b068
Karthik AG <karthikjr2004jr@gmail.com>
2026-08-29 20:55:17 +0530

    Time: 1049 ms (44.1%), Space: 53.5 MB (86.11%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.js
+++ 0507-perfect-number/0507-perfect-number.js
@@ -3,10 +3,10 @@
  * @return {boolean}
  */
 var checkPerfectNumber = function(num) {
-   let n = num/2, sum = 0
-   for(let i = 1 ; i<=n;i++){
+   let sum = 0
+   for(let i = 1 ; i<=(num/2); i++){
     if(num%i === 0){
-        sum = sum + i
+        sum += i
     }
    }
 
