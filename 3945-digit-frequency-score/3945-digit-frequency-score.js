4578ee60aabbae77aec13f8631f4192b904d897e
Karthik AG <karthikjr2004jr@gmail.com>
2026-09-03 10:11:03 +0530

    Time: 0 ms (100%), Space: 56.3 MB (46.42%) - LeetHub

---
+++ 3945-digit-frequency-score/3945-digit-frequency-score.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var digitFrequencyScore = function(n) {
+    return n.toString().split("").reduce((sum , item)=>sum = sum+Number(item),0)
+    
+};
