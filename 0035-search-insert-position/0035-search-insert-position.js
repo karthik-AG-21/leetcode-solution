3fcacd6cd0ac079e2da42088040065be77ac4107
Karthik AG <karthikjr2004jr@gmail.com>
2026-09-07 09:59:45 +0530

    Time: 0 ms (100%), Space: 51 MB (100%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let index = 0
+    if(nums.includes(target)){
+        return nums.indexOf(target)
+    }else if(!nums.includes(target) && nums[nums.length-1] <= target ){
+        return nums.length
+    }
+
+    for(let i = 0; i<nums.length; i++){
+        if(!nums.includes(target) && nums[i] > target ){
+            return i
+        }
+    }
+
+   
+};
