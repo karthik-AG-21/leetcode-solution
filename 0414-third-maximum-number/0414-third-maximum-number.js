05dc78459bc4f5f9128e7f45c1df2b45e069ead6
Karthik AG <karthikjr2004jr@gmail.com>
2026-09-10 20:08:24 +0530

    Time: 0 ms (100%), Space: 55.5 MB (60.12%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.js
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -7,16 +7,11 @@ var thirdMax = function(nums) {
     let num = [...array].sort((a,b)=>b-a);
 
     if(num.length>=3){
-
         return num[2];
-
     }else if(num.length<=2){
-
         return Math.max(...num);
-
     }else{
         return -1;
     }
 
-    
 };
