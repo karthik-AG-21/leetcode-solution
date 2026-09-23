973616274846578bfcf02f9fe09634f4daf6c7c5
Karthik AG <karthikjr2004jr@gmail.com>
2026-09-23 09:48:00 +0530

    Time: 3 ms (9.98%), Space: 52.6 MB (90.43%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.js
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -4,24 +4,18 @@
  */
 var isPerfectSquare = function(num) {
     let n = 0;
-
     if(num == 1 ){
-            return true
-        }
+        return true
+    }
         if(num > 100000){
             n = 100000
         }else {
             n = num/2
-        }
-        
+        }     
     for(let i = 2; i<= n; i++ ){
-        console.log(i*i , num);
-
         if(i*i == num){
             return true
-        }
-
-        
+        }  
     }
     return false
 };
